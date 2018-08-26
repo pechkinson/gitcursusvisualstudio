b9331f775de3c21722a75dcea6202b639ae90ce9
Vladimir Pechnikov <v.pechnikov@gmail.com>
2018-08-26 16:39:47 +0200

    bonjour

--- GitCursus/Program.cs
+++ GitCursus/Program.cs
@@ -10,7 +10,7 @@ namespace GitCursus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo World");
+            Console.WriteLine("Bonjour toutlemond");
         }
     }
 }
